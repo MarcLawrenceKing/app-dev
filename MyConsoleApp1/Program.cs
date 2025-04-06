7061d52389c265cb4a7a4c5f16a717ae08be68c1
MarcLawrenceKing <124873963+MarcLawrenceKing@users.noreply.github.com>
2025-04-06 16:46:31 +0800

    first commit

---
+++ MyConsoleApp1/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MyConsoleApp1
+{
+    internal class Program
+    {
+        static void MyMethod()
+        {
+            Console.WriteLine("I just got executed!");
+        }
+        static void Main(string[] args)
+        {
+            MyMethod();
+            MyMethod();
+            MyMethod();
+            MyMethod();
+        }
+    }
+}
